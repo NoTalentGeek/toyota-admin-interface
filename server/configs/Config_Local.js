ebc6faacae34ea0c652e19625abcb92fcda108ca
notalentgeek <mikael.pratama@yahoo.com>
2016-08-22 22:28:46 +0700

    Save, added a feature to let the application load database from local first then from remote MongoDB database. Also moved the passport so that it init after the database initiation.

--- server/configs/Config_Local.js
+++ server/configs/Config_Local.js
@@ -1,5 +1,4 @@
 //Database URL.
 module.exports = {
-    //"url": "mongodb://notalentgeek:asd@ds044229.mlab.com:44229/database_toyota_admin_interface_mean"
     "url": "mongodb://localhost:27017/database_toyota_admin_interface_mean"
 };
