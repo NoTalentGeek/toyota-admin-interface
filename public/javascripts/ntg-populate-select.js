e26fa748ed7e08666104b538ff4854602efd7326
notalentgeek <mikael.pratama@yahoo.com>
2016-08-25 13:27:26 +0700

    Save, before change the SocketIO from Bower.

--- public/javascripts/ntg-populate-select.js
+++ public/javascripts/ntg-populate-select.js
@@ -1,5 +1,5 @@
 //Car.
-var Object_Result_Car = undefined;
+var Object_ResultCar = undefined;
 angular.module("ng_app_page_edit_register_car", [])
 .controller(
     "ng_controller_page_edit_register_car",
@@ -17,21 +17,21 @@ angular.module("ng_app_page_edit_register_car", [])
         .success(
             function (_Object_Result){
                 $scope.ng_options_select_car = _Object_Result;
-                Object_Result_Car = _Object_Result;
+                Object_ResultCar = _Object_Result;
             }
         );
     }
 );
 $("#ntg-select-car").change(function(){
     var Number_Index = $("#ntg-select-car").prop("selectedIndex");
-    $("#Car_String_Name_Edit").val(Object_Result_Car[Number_Index - 1].Car_String_Name);
+    $("#Car_String_Name_Edit").val(Object_ResultCar[Number_Index - 1].Car_String_Name);
 });
 
 
 
 
 //User.
-var Object_Result_User = undefined;
+var Object_ResultUser = undefined;
 angular.module("ng_app_page_edit_register_user", [])
 .controller(
     "ng_controller_page_edit_register_user",
@@ -49,15 +49,15 @@ angular.module("ng_app_page_edit_register_user", [])
         .success(
             function (_Object_Result){
                 $scope.ng_options_select_user = _Object_Result;
-                Object_Result_User = _Object_Result;
+                Object_ResultUser = _Object_Result;
             }
         );
     }
 );
 $("#ntg-select-user").change(function(){
     var Number_Index = $("#ntg-select-user").prop("selectedIndex");
-    $("#User_String_Email_Edit").val(Object_Result_User[Number_Index - 1].User_String_Email);
-    $("#User_String_Name_Edit").val(Object_Result_User[Number_Index - 1].User_String_Name);
+    $("#User_String_Email_Edit").val(Object_ResultUser[Number_Index - 1].User_String_Email);
+    $("#User_String_Name_Edit").val(Object_ResultUser[Number_Index - 1].User_String_Name);
 });
 
 
@@ -65,7 +65,7 @@ $("#ntg-select-user").change(function(){
 
 
 //Workshop.
-var Object_Result_Workshop = undefined;
+var Obejct_ResultWorkshop = undefined;
 angular.module("ng_app_page_edit_register_workshop", [])
 .controller(
     "ng_controller_page_edit_register_workshop",
@@ -83,7 +83,7 @@ angular.module("ng_app_page_edit_register_workshop", [])
         .success(
             function (_Object_Result){
                 $scope.ng_options_select_workshop = _Object_Result;
-                Object_Result_Workshop = _Object_Result;
+                Obejct_ResultWorkshop = _Object_Result;
             }
         );
     }
@@ -92,27 +92,27 @@ angular.module("ng_app_page_edit_register_workshop", [])
 //    also move the marker into the currently seelcted workshop.
 $("#ntg-select-workshop").change(function(){
     var Number_Index = $("#ntg-select-workshop").prop("selectedIndex");
-    $("#Workshop_Number_Latitude_Edit").val(Object_Result_Workshop[Number_Index - 1].Workshop_Number_Latitude);
-    $("#Workshop_Number_Longitude_Edit").val(Object_Result_Workshop[Number_Index - 1].Workshop_Number_Longitude);
-    $("#Workshop_String_Name_Edit").val(Object_Result_Workshop[Number_Index - 1].Workshop_String_Name);
+    $("#Workshop_Number_Latitude_Edit").val(Obejct_ResultWorkshop[Number_Index - 1].Workshop_Number_Latitude);
+    $("#Workshop_Number_Longitude_Edit").val(Obejct_ResultWorkshop[Number_Index - 1].Workshop_Number_Longitude);
+    $("#Workshop_String_Name_Edit").val(Obejct_ResultWorkshop[Number_Index - 1].Workshop_String_Name);
 
     if(Object_Marker_EditRegisterWorkshop_1 === undefined){
         Object_Marker_EditRegisterWorkshop_1 = new google.maps.Marker({
             map: Object_Map_EditRegisterWorkshop_1,
             position: new google.maps.LatLng(
-                Object_Result_Workshop[Number_Index - 1].Workshop_Number_Latitude,
-                Object_Result_Workshop[Number_Index - 1].Workshop_Number_Longitude
+                Obejct_ResultWorkshop[Number_Index - 1].Workshop_Number_Latitude,
+                Obejct_ResultWorkshop[Number_Index - 1].Workshop_Number_Longitude
             )
         });
     }
     else{
         Object_Marker_EditRegisterWorkshop_1.setPosition(new google.maps.LatLng(
-            Object_Result_Workshop[Number_Index - 1].Workshop_Number_Latitude,
-            Object_Result_Workshop[Number_Index - 1].Workshop_Number_Longitude
+            Obejct_ResultWorkshop[Number_Index - 1].Workshop_Number_Latitude,
+            Obejct_ResultWorkshop[Number_Index - 1].Workshop_Number_Longitude
         ));
     }
     Object_Map_EditRegisterWorkshop_1.panTo(new google.maps.LatLng(
-        Object_Result_Workshop[Number_Index - 1].Workshop_Number_Latitude,
-        Object_Result_Workshop[Number_Index - 1].Workshop_Number_Longitude
+        Obejct_ResultWorkshop[Number_Index - 1].Workshop_Number_Latitude,
+        Obejct_ResultWorkshop[Number_Index - 1].Workshop_Number_Longitude
     ));
 });
