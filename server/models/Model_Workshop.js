c80474736602013a6ce6d190de5baa33c34a7d7f
notalentgeek <mikael.pratama@yahoo.com>
2016-08-30 21:10:45 +0700

    Save before adding forms.

--- server/models/Model_Workshop.js
+++ server/models/Model_Workshop.js
@@ -1,13 +1,25 @@
 var Object_Mongoose = require("mongoose");
 var Object_Schema = Object_Mongoose.Schema;
 var Object_Schema_Workshop = new Object_Schema({
-    Workshop_Array_String_SlotFriday: { default: [], type: Array },
-    Workshop_Array_String_SlotMonday: { default: [], type: Array },
-    Workshop_Array_String_SlotSaturday: { default: [], type: Array },
-    Workshop_Array_String_SlotSunday: { default: [], type: Array },
-    Workshop_Array_String_SlotThursday: { default: [], type: Array },
-    Workshop_Array_String_SlotTuesday: { default: [], type: Array },
-    Workshop_Array_String_SlotWednesday: { default: [], type: Array },
+
+
+
+
+    //For the workshop slot it will be filled with a JavaScript "struct"
+    //    the "struct" will be filled with Boolean_Occupied, String_TimeEnd, and
+    //    String_TimeStart,
+    Workshop_Array_Struct_SlotFriday: { default: [], type: Array },
+    Workshop_Array_Struct_SlotMonday: { default: [], type: Array },
+    Workshop_Array_Struct_SlotSaturday: { default: [], type: Array },
+    Workshop_Array_Struct_SlotSunday: { default: [], type: Array },
+    Workshop_Array_Struct_SlotThursday: { default: [], type: Array },
+    Workshop_Array_Struct_SlotTuesday: { default: [], type: Array },
+    Workshop_Array_Struct_SlotWednesday: { default: [], type: Array },
+
+
+
+
+
     Workshop_Number_Latitude: { default: 0.0, type: Number },
     Workshop_Number_Longitude: { default: 0.0, type: Number },
     Workshop_String_Name: { default: "", type: String }
