8fd6357936b01f4f2d8d52ff9417004736c49ef4
notalentgeek <mikael.pratama@yahoo.com>
2016-09-02 11:38:17 +0700

    Delete database everytime it runs new.

--- server.js
+++ server.js
@@ -137,6 +137,11 @@ Object_Database = Object_Mongoose.connect(Object_Config_MongoDB.url, function(_O
         throw _Object_Error;
     }
 
+    //For debugging I want so that when the web application starts it drops the
+    //    database and then re - create new.
+    console.log("For development purposes I delete database everytime it runs new.");
+    Object_Mongoose.connection.db.dropDatabase();
+
     //If connection is successfull then check to see if
     //    there is an element in database.
     Void_CheckCollectionEmpty(Array_Model_, 0);
